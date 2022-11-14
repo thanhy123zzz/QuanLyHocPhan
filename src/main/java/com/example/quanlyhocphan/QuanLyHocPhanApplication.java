bb3ac93c5cce0596c6f6e8681fe3d86d7c746893
Thanh Y <thanhy123zzz@gmail.com>
2022-11-14 22:52:07 +0700

    upload first

---
+++ src/main/java/com/example/quanlyhocphan/QuanLyHocPhanApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlyhocphan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyHocPhanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyHocPhanApplication.class, args);
+    }
+
+}
