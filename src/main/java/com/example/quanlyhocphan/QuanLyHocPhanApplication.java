38ad01cdc8653d8daeac38c58752966d23db2caf
Thanh Y <thanhy123zzz@gmail.com>
2022-11-15 14:54:32 +0700

    Update Layout

--- src/main/java/com/example/quanlyhocphan/QuanLyHocPhanApplication.java
+++ src/main/java/com/example/quanlyhocphan/QuanLyHocPhanApplication.java
@@ -3,7 +3,7 @@ package com.example.quanlyhocphan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.quanlyhocphan")
 public class QuanLyHocPhanApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/quanlyhocphan/Service/EmailService.java
@@ -0,0 +1,11 @@
+package com.example.quanlyhocphan.Service;
+
+public interface EmailService {
+    // Method
+    // To send a simple email
+    String sendSimpleMail();
+
+    // Method
+    // To send an email with attachment
+    String sendMailWithAttachment();
+}
